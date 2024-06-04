ba1279fc454d2da375f9652fabacf5a518e4851f
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-05 06:12:13 +0800

    Learne about public interface object

--- script.js
+++ script.js
@@ -332,32 +332,67 @@ class PersonCl {
 // martha.introduce();
 // martha.calcAge();
 
-const PersonProto = {
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  },
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  },
-};
-const steven = Object.create(PersonProto);
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+// const steven = Object.create(PersonProto);
 
-const StudentProto = Object.create(PersonProto);
-StudentProto.init = function (
-  firstName,
-  birthYear,
-  course
-) {
-  PersonProto.init.call(this, firstName, birthYear);
-  this.course = course;
-};
-StudentProto.introduce = function () {
-  console.log(
-    `My name is ${this.firstName} and I study ${this.course}`
-  );
-};
-const jay = Object.create(StudentProto);
-jay.init('Jay', 2010, 'Computer Science');
-jay.introduce();
-jay.calcAge();
+// const StudentProto = Object.create(PersonProto);
+// StudentProto.init = function (
+//   firstName,
+//   birthYear,
+//   course
+// ) {
+//   PersonProto.init.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+// StudentProto.introduce = function () {
+//   console.log(
+//     `My name is ${this.firstName} and I study ${this.course}`
+//   );
+// };
+// const jay = Object.create(StudentProto);
+// jay.init('Jay', 2010, 'Computer Science');
+// jay.introduce();
+// jay.calcAge();
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${owner} `);
+  }
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdrawal(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+const acc1 = new Account('Jonas', 'EUR', 1111);
+acc1.deposit(250);
+acc1.withdrawal(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
