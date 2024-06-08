bf6e47e960269abd9e7da67eb5f2ea7850818e9a
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-08 13:53:42 +0800

    Learned about Private class fields and methods

--- script.js
+++ script.js
@@ -363,40 +363,54 @@ class PersonCl {
 // jay.calcAge();
 
 class Account {
+  // Public fields (instances)
+  locale = navigator.language;
+
+  // Privates fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account ${owner} `);
   }
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
   withdrawal(val) {
     this.deposit(-val);
   }
-  _approveLoan(val) {
-    return true;
-  }
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+  static helper() {
+    console.log('Helper');
   }
 }
 const acc1 = new Account('Jonas', 'EUR', 1111);
 acc1.deposit(250);
 acc1.withdrawal(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+// acc1._approveLoan(1000);
 console.log(acc1.getMovements());
-
 console.log(acc1);
-console.log(acc1.pin);
+// console.log(acc1.#pin);
+// console.log(acc.#movements);
+// console.log(acc1.#approveLoan(100));
+Account.helper;
