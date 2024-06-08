d6fb949a21d002e4bbe9331ca662ab463d1b40bd
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-08 13:16:38 +0800

    Using convention way of encapsulation

--- script.js
+++ script.js
@@ -366,19 +366,22 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account ${owner} `);
   }
+  getMovements() {
+    return this._movements;
+  }
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
   withdrawal(val) {
     this.deposit(-val);
   }
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
   requestLoan(val) {
@@ -393,5 +396,7 @@ acc1.deposit(250);
 acc1.withdrawal(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
+console.log(acc1.pin);
